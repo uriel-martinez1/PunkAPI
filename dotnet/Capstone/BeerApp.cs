a5a2ab50d93718686e77578515d5ba00b92a1818
uriel-martinez1 <martinezu444@gmail.com>
2024-02-07 16:18:12 -0500

    Added the RestSharp nuget package to the project

---
+++ dotnet/Capstone/BeerApp.cs
@@ -0,0 +1,16 @@
+﻿using Capstone.Services;
+using Capstone.Models;
+
+namespace Capstone
+{
+    public class BeerApp
+    {
+        private readonly PunkApiService punkApiService;
+
+
+        public BeerApp(string apiURL)
+        {
+            this.punkApiService = new PunkApiService(apiURL);
+        }
+    }
+}
--- dotnet/Capstone/Services/PunkApiService.cs
+++ dotnet/Capstone/Services/PunkApiService.cs
@@ -1,7 +1,14 @@
-﻿namespace Capstone.Services
+﻿using RestSharp; 
+
+namespace Capstone.Services
 {
     public class PunkApiService
     {
+        private string apiURL;
 
+        public PunkApiService(string apiURL)
+        {
+            this.apiURL = apiURL;
+        }
     }
 }
